73fe71455b6cb1b70305ce3fa25769e1dbc67b3e
Haruki Miyagi <haruki.miyagi11@gmail.com>
2023-09-14 14:14:45 +0900

    protoc でjs,tsコード自動生成

--- generate.sh
+++ generate.sh
@@ -6,4 +6,6 @@ mkdir -p ${CLIENT_OUTDIR} ${SERVER_OUTPUT_DIR}
 protoc \
       --go_out=${SERVER_OUTPUT_DIR} \
       --go-grpc_out=${SERVER_OUTPUT_DIR} \
+      --js_out=import_style=commonjs:${CLIENT_OUTDIR} \
+      --grpc-web_out=import_style=typescript,mode=grpcwebtext:${CLIENT_OUTDIR} \
       ./proto/*.proto
