393e40a562c785528049eafbeecb4684216c2bde
Haruki Miyagi <haruki.miyagi11@gmail.com>
2023-09-14 11:48:42 +0900

    go serverを作成

---
+++ server/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/hrk-m/grpc-web-sample/server/greet"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":9090"
+)
+
+type server struct {
+	pb.UnimplementedGreetServiceServer
+}
+
+func (s *server) Greet(ctx context.Context, in *pb.GreetRequest) (*pb.GreetResponse, error) {
+	log.Println("--------------------------------------")
+	log.Println("ctx:", ctx)
+	log.Println("pb:", in)
+
+	return &pb.GreetResponse{Message: "Hello " + in.Name + " !"}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGreetServiceServer(s, &server{})
+
+	reflection.Register(s)
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
