1f3e011c53a917f24b77c9aa245dcb003c2fd198
Haruki Miyagi <haruki.miyagi11@gmail.com>
2023-09-14 14:15:26 +0900

    画面に表示してみた

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { GreetRequest } from "./proto/greet_pb";
+import { GreetServiceClient } from "./proto/GreetServiceClientPb";
+
+const initialState = {
+  inputText: "World",
+  message: ""
+};
+type State = Readonly<typeof initialState>;
+
+class App extends React.Component<{}, State> {
+  public readonly state: State = initialState;
+
+  public render() {
+    return (
+      <div className="App">
+        <input
+          type="text"
+          value={this.state.inputText}
+          onChange={this.onChange}
+        />
+        <button onClick={this.onClick}>Send</button>
+        <p>{this.state.message}</p>
+      </div>
+    );
+  }
+
+  private onClick = () => {
+    const request = new GreetRequest();
+    request.setName(this.state.inputText);
+
+    const client = new GreetServiceClient("http://localhost:8080", {}, {});
+    client.greet(request, {}, (err, ret) => {
+      if (err || ret === null) {
+        throw err;
+      }
+      this.setState({ message: ret.getMessage() });
+    });
+  };
+
+  private onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ inputText: e.target.value });
+  };
 }
 
 export default App;
